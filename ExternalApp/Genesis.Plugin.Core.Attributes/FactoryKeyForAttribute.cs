70cb85d2d4f971909aeb93541930a302226c5f28
Jeff Campbell <jeff@zapdot.com>
2020-09-29 08:36:36 -0400

    Created example attributes dll
    * Created example .Net Standard attributes dll in Genesis solution; it contains attributes used by both code generator plugins in the CLI app as well as Unity. Post-build event copies this to local Unity solution.

---
+++ ExternalApp/Genesis.Plugin.Core.Attributes/FactoryKeyForAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Genesis.Plugin.Core.Attributes
+{
+	/// <summary>
+	/// Indicates that a scriptable factory should be created using the decorated class as a key to
+	/// <see cref="ValueType"/>.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+	public sealed class FactoryKeyForAttribute : Attribute
+	{
+		public Type ValueType { get; }
+
+		public FactoryKeyForAttribute(Type valueType)
+		{
+			ValueType = valueType;
+		}
+	}
+}
